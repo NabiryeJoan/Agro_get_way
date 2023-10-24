2573838b70c6900736d411e7ade3fef0b70140a6
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2023-10-24 21:17:40 +0300

    intialize

--- agroget_away_2023-10-23_190458.sql
+++ agroget_away_2023-10-23_190458.sql
@@ -1,38 +1,53 @@
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET SQL_NOTES=0 */;
-DROP TABLE IF EXISTS farmer;
-CREATE TABLE `farmer` (
-  `farm_id` int(11) NOT NULL AUTO_INCREMENT,
-  `farm_name` varchar(255) NOT NULL,
-  `location` varchar(255) DEFAULT NULL,
-  `crop_type` varchar(255) DEFAULT NULL,
-  `registration_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`farm_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Active: 1698070377093@@127.0.0.1@3306
+CREATE DATABASE Agroget_away;
+use Agroget_away;
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contact VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255) NOT NULL
+);
 
-DROP TABLE IF EXISTS interactions;
-CREATE TABLE `interactions` (
-  `interaction_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `farm_id` int(11) DEFAULT NULL,
-  `type_id` int(11) DEFAULT NULL,
-  `interaction_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `comments` text DEFAULT NULL,
-  PRIMARY KEY (`interaction_id`),
-  KEY `user_id` (`user_id`),
-  KEY `farm_id` (`farm_id`),
-  CONSTRAINT `interactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  CONSTRAINT `interactions_ibfk_2` FOREIGN KEY (`farm_id`) REFERENCES `farmer` (`farm_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `contact_info` varchar(255) DEFAULT NULL,
-  `registration_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE farmer (
+    farmer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contact VARCHAR(255),
+    type VARCHAR(255)
+);
+CREATE TABLE farm (
+    farm_id INT AUTO_INCREMENT PRIMARY KEY,
+    farm_name VARCHAR(255) NOT NULL,
+    location VARCHAR(255),
+    type VARCHAR(255)
+);
 
+CREATE TABLE booking (
+    booking_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    farm_id INT,
+    date_and_time TIMESTAMP,
+    farmer_id INT
+    -- FOREIGN KEY (user_id) REFERENCES users(user_id),
+    -- FOREIGN KEY (farm_id) REFERENCES farms(farm_id),
+    -- FOREIGN KEY (farmer_id) REFERENCES farmers(farmer_id)
+);
+CREATE TABLE comment (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    text TEXT,
+    farm_id INT,
+    farmer_id INT,
+    user_id INT,
+    date_and_time TIMESTAMP
+    -- FOREIGN KEY (farm_id) REFERENCES farms(farm_id),
+    -- FOREIGN KEY (farmer_id) REFERENCES farmers(farmer_id),
+    -- FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+ALTER TABLE  booking ADD FOREIGN KEY (user_id) REFERENCES user(user_id);
+ALTER TABLE  booking ADD FOREIGN KEY (farm_id) REFERENCES farm(farm_id);
+ALTER TABLE  booking ADD FOREIGN KEY (farmer_id) REFERENCES farmer(farmer_id);
+
+ALTER TABLE  comment ADD FOREIGN KEY (farm_id) REFERENCES farm(farm_id);
+ALTER TABLE  comment ADD FOREIGN KEY (farmer_id) REFERENCES farmer(farmer_id);
+ALTER TABLE  comment ADD FOREIGN KEY (user_id) REFERENCES user(user_id)
