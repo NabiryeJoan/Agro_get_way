8152905ca87f52026f2e38cf6b885f9b2d044d0c
Tryphine <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2023-10-25 11:54:35 +0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+$manager = new MongoDB\Driver\Manager("mongodb+srv://admin:Truth@cluster0.69xowuj.mongodb.net/");
+
+// Select a specific database
+$database = "Agrogetaway";
+
+// Select a specific collection
+$collection = "users";
+
+?>
