03b6023f96730447cfadc5e745a7a4443dae38f5
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2023-10-26 17:23:19 +0300

    eeeeeee

--- register.php
+++ register.php
@@ -1,60 +1,68 @@
+
+
 <?php
-require 'database.php';
-$collection = (new Mysql\Client)->Agrogetaway->users;
+session_start();
+error_reporting(0);
 
-// Check if the form was submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve user input
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+include('db_config.php');
+error_reporting(0);
+if (isset($_POST['submit'])) {
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $contact = $_POST['contact'];
+      $password = $_POST['password'];
 
-    // Validate user input
-    $errors = [];
 
-    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Please provide a valid email address.";
-    }
-
-    if (empty($username)) {
-        $errors[] = "Username is required.";
-    }
-
-    if (empty($password)) {
-        $errors[] = "Password is required.";
-    }
-
-    // Check for existing username
-    if ($collection->findOne(['username' => $username])) {
-        $errors[] = "Username already exists. Please choose a different one.";
-    }
-
-    if (empty($errors)) {
-        // Hash the password
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-        // Create a new user document
-        $user = [
-            'username' => $username,
-            'password' => $passwordHash,
-            'email' => $email,
-        ];
-
-        // Insert the user document into the database
-        $result = $collection->insertOne($user);
-
-        if ($result->getInsertedCount() > 0) {
-            // Registration successful
-            echo "Registration successful. You can now log in.";
-        } else {
-            // Handle database insertion error
-            echo "An error occurred during registration. Please try again.";
-        }
-    } else {
-        // Display validation errors
-        foreach ($errors as $error) {
-            echo $error . "<br>";
-        }
-    }
+      $ret=mysqli_query($con,"INSERT INTO users (name, email, contact, password) VALUES ('$name', '$email', '$contact', '$password')");
+      $result=mysqli_fetch_array($ret);
 }
-?>
+
+    // Check if the form was submitted
+    // if ($_['REQUEST_METHOD'] === 'POST') {
+
+    //   // Get the user's input
+    //   $name = $_POST['name'];
+    //   $email = $_POST['email'];
+    //   $contact = $_POST['contact'];
+    //   $password = $_POST['password'];
+
+      // Validate the user's input
+    //   $errors = [];
+    //   if (empty($name)) {
+    //     $errors[] = 'Please enter your full name.';
+    //   }
+    //   if (empty($email)) {
+    //     $errors[] = 'Please enter your email address.';
+    //   } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //     $errors[] = 'Please enter a valid email address.';
+    //   }
+    //   if (empty($contact)) {
+    //     $errors[] = 'Please enter your contact number.';
+    //   } elseif (!preg_match('/^\d{10}$/', $contact)) {
+    //     $errors[] = 'Please enter a valid contact number.';
+    //   }
+    //   if (empty($password)) {
+    //     $errors[] = 'Please enter a password.';
+    //   } elseif (strlen($password) < 8) {
+    //     $errors[] = 'Password must be at least 8 characters long.';
+    //   } elseif ($password !== $_POST['confirm_password']) {
+    //     $errors[] = 'Password and confirm password do not match.';
+    //   }
+
+      // If there are no errors, insert the user's information into the database
+    //   if (empty($errors)) {
+    //     $sql = "INSERT INTO users (name, email, contact, password) VALUES ('$name', '$email', '$contact', '$password')";
+    //     $result = $conn->query($sql);
+    //     if ($result) {
+    //       header('Location: login.php');
+    //       exit;
+    //     } else {
+    //       echo '<p class="error">Registration failed. Please try again.</p>';
+    //     }
+    //   } else {
+    //     foreach ($errors as $error) {
+    //       echo '<p class="error">' . $error . '</p>';
+    //     }
+    //   }
+////////////
+  ?>
--- signup.php
+++ signup.php
@@ -6,14 +6,21 @@
 <body>
     <h1>Sign Up</h1>
     <form action="register.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+        
+
+
 
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br>
 
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br>
+
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br>
+
+        <label for="contact">Contact:</label>
+        <input type="contact" id="contact" name="contact" required><br>
 
         <input type="submit" value="Sign Up">
     </form>
